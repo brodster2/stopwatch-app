174f76911e290395a0d953da85bd656fd9c45a61
Brodie <brodster22@live.com>
2017-03-16 14:49:47 +0000

    Add variables and hideShowButtons() function

--- logic.js
+++ logic.js
@@ -1,15 +1,16 @@
 $(function(){
     
     //Variables
-        //App mode
-        //time counter
-        //lap counter
-        //variable for setInterval
-        //Number of laps
-        //minutes, seconds, centi seconds for time and lap
+    var mode = 0;//App mode
+    var timeCounter = 0;//time counter
+    var lapCounter = 0;//lap counter
+    var action;//variable for setInterval
+    var lapNumber = 0//Number of laps
+    //minutes, seconds, centi seconds for time and lap
+    var timeMinutes, timeSeconds, timeCentiseconds, lapMinutes, lapSeconds, lapCentiseconds;
     
     //On App load show start and lap buttons
-    
+    hideShowButtons("#startButton", "#lapButton");
     //click on startButton
         //mode on
         //show stop and lap buttons
@@ -36,4 +37,9 @@ $(function(){
             //start timer
     
     //functions
+    function hideShowButtons(btnId1, btnId2){
+        $('.control').hide();
+        $(btnId1).show();
+        $(btnId2).show();
+    }
 });
