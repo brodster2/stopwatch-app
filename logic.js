46c3fc51b81359730f023d27c28611848c9d4c83
Brodie <brodster22@live.com>
2017-03-16 15:47:30 +0000

    Add button fuctionality and number formatting
    Add stopButton functionallity and format() to append 0 before time numbers if less than 0.

--- logic.js
+++ logic.js
@@ -13,18 +13,22 @@ $(function(){
     hideShowButtons("#startButton", "#lapButton");
     //click on startButton
     $('#startButton').click(function(){
+        //mode on
         mode = 1;
+        //show stop and lap buttons
         hideShowButtons("#stopButton", "#lapButton");
+        //start counter
         startAction();
     });
-        //mode on
-        //show stop and lap buttons
-        //start counter
     
     //click on stopButton
+    $('#stopButton').click(function(){
         //show resume and reset buttons
+        hideShowButtons("#resumeButton", "#resetButton");
         //stop counter
-    
+        clearInterval(action);
+    });
+        
     //click on resumeButton
         //show stop and lap buttons
         //resume counter
@@ -63,15 +67,34 @@ $(function(){
     }
     
     /**
-    * Divide lap/time counters to produce min,sec,centisec elapsed.
+    * Divide lap/time counters to produce min,sec,centisec elapsed and display them.
     **/
     function updateTime(){
         timeMinutes = Math.floor(timeCounter/6000);
         timeSeconds = Math.floor((timeCounter%6000)/100);
         timeCentiseconds = Math.floor(timeCounter%6000)%100;
         
+        $('#timeminute').text(format(timeMinutes));
+        $('#timesecond').text(format(timeSeconds));
+        $('#timecentisecond').text(format(timeCentiseconds));
+        
         lapMinutes = Math.floor(lapCounter/6000);
         lapSeconds = Math.floor((lapCounter%6000)/100);
         lapCentiseconds = Math.floor(lapCounter%6000)%100;
+        
+        $('#lapminute').text(format(lapMinutes));
+        $('#lapsecond').text(format(lapSeconds));
+        $('#lapcentisecond').text(format(lapCentiseconds));
+    }
+    
+    /**
+    * Adds a 0 before number if less than 10
+    **/
+    function format(number){
+        if(number < 10){
+            return "0" + number;
+        } else {
+            return number;
+        }
     }
 });
