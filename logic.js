3849712107eb93ecb8d8609454bdb248e776ed22
Brodie <brodster22@live.com>
2017-03-16 14:09:09 +0000

    Add initial html, css

---
+++ logic.js
@@ -0,0 +1,14 @@
+$(function(){
+    
+    /*$('#startButton').click(function(){
+        timer = setInterval(function(){
+            cent++;
+            $('#cent').val(cent);
+        },100);
+    })*/
+    
+    /*Stop timer*/
+    /*$('#stopButton').click(function(){
+        clearInterval(timer);
+    })*/
+});
