eb0685585e2f283bff383818975f1be69f6a0de2
Brodie <brodster22@live.com>
2017-03-16 14:32:56 +0000

    Add jQuery code descriptions

--- logic.js
+++ logic.js
@@ -1,14 +1,39 @@
 $(function(){
     
-    /*$('#startButton').click(function(){
-        timer = setInterval(function(){
-            cent++;
-            $('#cent').val(cent);
-        },100);
-    })*/
+    //Variables
+        //App mode
+        //time counter
+        //lap counter
+        //variable for setInterval
+        //Number of laps
+        //minutes, seconds, centi seconds for time and lap
     
-    /*Stop timer*/
-    /*$('#stopButton').click(function(){
-        clearInterval(timer);
-    })*/
+    //On App load show start and lap buttons
+    
+    //click on startButton
+        //mode on
+        //show stop and lap buttons
+        //start counter
+    
+    //click on stopButton
+        //show resume and reset buttons
+        //stop counter
+    
+    //click on resumeButton
+        //show stop and lap buttons
+        //resume counter
+    
+    //click on resetButton
+        //reload the page
+    $('#resetButton').click(function(){
+        location.reload();
+    });
+    
+    //click on lapButton
+        //if mode is on
+            //stop timer
+            //resetLap and print lap details
+            //start timer
+    
+    //functions
 });
